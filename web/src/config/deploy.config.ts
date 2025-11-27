1c5d1e0c2d8e88ca9fd1a6b9276ece1a93189579
Cody Sheridan <cody.sheridan@proton.me>
2025-11-26 20:21:47 -0500

    update deploy config for azure swa

--- web/src/config/deploy.config.ts
+++ web/src/config/deploy.config.ts
@@ -1,15 +1,16 @@
 /** Output folder for the website build */
-export const BUILD_DIR = "dist";
+export const BUILD_DIR = 'dist';
 
 /** Domain to deploy the website to */
-export const DOMAIN = "os.prozilla.dev";
+export const DOMAIN = 'orange-grass-03e4cc80f.3.azurestaticapps.net';
 /** Base URL of the website, that consists of the http protocol and the domain */
 export const BASE_URL = `https://${DOMAIN}/`;
 
 /** Properties of the GitHub repository with the source code of the website */
-export const REPO = { owner: "prozilla-os", name: "ProzillaOS", branch: "main" };
+export const REPO = { owner: 'kuhlekt1v', name: 'Kuhlekt1vOS', branch: 'main' };
 /** URL of the GitHub repository with the source code of the website */
 export const REPO_URL = `https://github.com/${REPO.owner}/${REPO.name}`;
 
 /** Commit message for GitHub Pages deployments */
-export const COMMIT_MESSAGE = "Deployed build to GitHub Pages";
+export const COMMIT_MESSAGE = 'Deployed build to GitHub Pages';
+
