a0bb38d726bb82b7cfbcb13c55506027181812d2
Cody Sheridan <cody.sheridan@proton.me>
2025-11-26 21:00:06 -0500

    modifying files for github action

--- web/src/config/virtualDrive.config.ts
+++ web/src/config/virtualDrive.config.ts
@@ -1,20 +1,21 @@
-import { VirtualDriveConfig, VirtualFolder, VirtualRoot } from "prozilla-os";
+import { VirtualDriveConfig, VirtualFolder, VirtualRoot } from 'prozilla-os';
 
 function loadData(virtualRoot: VirtualRoot) {
-	const desktopFolder = virtualRoot.navigate("~/Desktop");
+  const desktopFolder = virtualRoot.navigate('~/Desktop');
 
-	if (!desktopFolder || !(desktopFolder instanceof VirtualFolder)) return;
+  if (!desktopFolder || !(desktopFolder instanceof VirtualFolder)) return;
 
-	desktopFolder.createFile("About", "md", (file) => {
-		file.setSource("documents/about.md");
-	});
+  desktopFolder.createFile('About', 'md', (file) => {
+    file.setSource('documents/about.md');
+  });
 }
 
 export const virtualDriveConfig = new VirtualDriveConfig({
-	saveData: false,
-	defaultData: {
-		includeAudioFolder: false,
-		includeVideoFolder: false,
-		loadData,
-	},
+  saveData: false,
+  defaultData: {
+    includeAudioFolder: false,
+    includeVideoFolder: false,
+    includeSourceTree: false,
+    loadData,
+  },
 });
